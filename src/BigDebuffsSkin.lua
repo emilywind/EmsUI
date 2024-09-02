a87dab1bb30aad47830379337ead3067b4ba6f8e
Emma Chatley <emchatley@gmail.com>
2024-09-02 14:04:56 -0400

    Add fix for SUI's missing raidframe texture config

--- src/BigDebuffsSkin.lua
+++ src/BigDebuffsSkin.lua
@@ -4,21 +4,6 @@ RillyCleanBigDebuffs:RegisterEvent("PLAYER_LOGIN")
 RillyCleanBigDebuffs:SetScript("OnEvent", function()
   if ( not C_AddOns.IsAddOnLoaded('BigDebuffs') ) then return end
 
-  -- Unit portraits
-  -- hooksecurefunc(BigDebuffs, 'AttachUnitFrame', function(self, unit)
-  --   local frameName = 'BigDebuffs' .. unit .. "UnitFrame"
-  --   local bdbFrame = _G[frameName]
-
-  --   local icon = bdbFrame.icon
-
-  --   if (icon) then
-  --     local cooldown = bdbFrame.cooldown or bdbFrame.Cooldown
-  --     icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-  --     cooldown:SetAllPoints(icon)
-  --     cooldown:SetSwipeTexture(SQUARE_TEXTURE)
-  --   end
-  -- end)
-
   -- Nameplates
   hooksecurefunc(BigDebuffs, 'NAME_PLATE_UNIT_ADDED', function(self, _, unit)
     local namePlate = C_NamePlate.GetNamePlateForUnit(unit)
