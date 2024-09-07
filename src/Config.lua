55f9884d9c1c738549139a31f181f8e15badb2ba
Emma Chatley <emlywind@gmail.com>
2024-09-07 00:32:30 -0400

    Add a button for reseting config to defaults

--- src/Config.lua
+++ src/Config.lua
@@ -41,35 +41,40 @@ EUIDBDefaults = {
   dampeningDisplay = true,
 }
 
-local function eui_defaults()
-  -- This function copies values from one table into another:
-  local function copyDefaults(src, dst)
-    -- If no source (defaults) is specified, return an empty table:
-    if type(src) ~= "table" then return {} end
-    -- If no target (saved variable) is specified, create a new table:
-    if type(dst) ~= "table" then dst = {} end
-    -- Loop through the source (defaults):
-    for k, v in pairs(src) do
-      -- If the value is a sub-table:
-      if type(v) == "table" then
-        -- Recursively call the function:
-        dst[k] = copyDefaults(v, dst[k])
-      -- Or if the default value type doesn't match the existing value type:
-      elseif type(v) ~= type(dst[k]) then
-        -- Overwrite the existing value with the default one:
-        dst[k] = v
-      end
+-- This function copies values from one table into another
+local function copyDefaults(src, dst)
+  -- If no source (defaults) is specified, return an empty table:
+  if type(src) ~= "table" then return {} end
+  -- If no target (saved variable) is specified, create a new table:
+  if type(dst) ~= "table" then dst = {} end
+  -- Loop through the source (defaults):
+  for k, v in pairs(src) do
+    -- If the value is a sub-table:
+    if type(v) == "table" then
+      -- Recursively call the function:
+      dst[k] = copyDefaults(v, dst[k])
+    -- Or if the default value type doesn't match the existing value type:
+    elseif type(v) ~= type(dst[k]) then
+      -- Overwrite the existing value with the default one:
+      dst[k] = v
     end
-    -- Return the destination table:
-    return dst
   end
+  -- Return the destination table:
+  return dst
+end
 
+local function eui_defaults()
   -- Copy the values from the defaults table into the saved variables table
   -- if it exists, and assign the result to the saved variable:
 
   EUIDB = copyDefaults(EUIDBDefaults, EUIDB)
 
   eui = {}
+end
+
+local function reset_defaults()
+  EUIDB = {}
+  EUIDB = copyDefaults(EUIDBDefaults, EUIDB)
 end
 
 local rc_catch = CreateFrame("Frame")
@@ -486,8 +491,16 @@ local function eui_options()
   )
 
   ------------------
-  --Reload Buttons --
+  --Reload Button --
   ------------------
+  local reload = CreateFrame("Button", "resettodefaults", eui.panel, "UIPanelButtonTemplate")
+  reload:SetPoint("BOTTOMLEFT", eui.panel, "BOTTOMLEFT", 10, 10)
+  reload:SetSize(120,22)
+  reload:SetText("Reset to Defaults")
+  reload:SetScript("OnClick", function()
+    reset_defaults()
+    ReloadUI()
+  end)
   local reload = CreateFrame("Button", "reload", eui.panel, "UIPanelButtonTemplate")
   reload:SetPoint("BOTTOMRIGHT", eui.panel, "BOTTOMRIGHT", -10, 10)
   reload:SetSize(100,22)
