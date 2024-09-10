ac001d3b4219b36372f1fd695fa4cb62d52d1c83
Emma Chatley <emlywind@gmail.com>
2024-09-10 06:23:59 -0400

    Use aura border texture for target/focus frame auras

--- src/Auras.lua
+++ src/Auras.lua
@@ -13,16 +13,9 @@ function applyAuraSkin(aura)
   --icon
   local icon = aura.Icon
 
-  icon:SetTexCoord(0.1,0.9,0.1,0.9)
-  icon:ClearAllPoints()
-  icon:SetPoint("TOPLEFT", aura, "TOPLEFT", 2, -2)
-  icon:SetPoint("BOTTOMRIGHT", aura, "BOTTOMRIGHT", -2, 2)
-
   --border
-  local border = aura:CreateTexture(aura.border, "BACKGROUND", nil, -7)
-  border:SetTexture(SQUARE_TEXTURE)
-  border:SetTexCoord(0,1,0,1)
-  border:SetDrawLayer("BACKGROUND",-7)
+  local border = aura:CreateTexture(aura.border, "OVERLAY")
+  border:SetTexture(EUI_TEXTURES.auraBorder)
 
   if aura.Border then
     border:SetVertexColor(aura.Border:GetVertexColor())
