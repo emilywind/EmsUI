a5246896ab4893325bdd3358b8148b625e821a4b
Emma Chatley <emlywind@gmail.com>
2024-09-10 09:10:21 -0400

    Prevent one-time flash of bag buttons when hoving over an npc

--- src/HideMicroMenuAndBags.lua
+++ src/HideMicroMenuAndBags.lua
@@ -21,8 +21,12 @@ function setBagBarVisibility()
       local bagButton =_G['CharacterBag' .. i .. 'Slot']
       -- Bag buttons have a strange interaction that causes them to show when hovering over NPCs unless done this way
       if not bagsVisible then
+        bagButton:SetAlpha(0)
+        bagButton:GetParent():SetAlpha(0)
         RegisterStateDriver(bagButton, "visibility", "hide")
       else
+        bagButton:GetParent():SetAlpha(1)
+        bagButton:SetAlpha(1)
         RegisterStateDriver(bagButton, "visibility", "show")
       end
     end
