7643a7d639ef6e22b8c08f2d093dbdd9f1c79dff
Emma Chatley <emlywind@gmail.com>
2024-09-13 22:07:30 -0400

    Skin boss frame cast bars

--- src/Castbars.lua
+++ src/Castbars.lua
@@ -56,6 +56,10 @@ for i = 1, 3 do
   _G['CompactArenaFrameMember' .. i].CastingBarFrame:HookScript("OnEvent", skinCastBar)
 end
 
+for i = 1, 5 do
+  _G['Boss' .. i .. 'TargetFrameSpellBar']:HookScript("OnEvent", skinCastBar)
+end
+
 TargetFrameSpellBar:HookScript("OnEvent", skinCastBar)
 FocusFrameSpellBar:HookScript("OnEvent", skinCastBar)
 
