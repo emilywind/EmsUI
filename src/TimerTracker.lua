7f625cc7ca69122b23450139f5fae0c9b9232429
Emma Chatley <emlywind@gmail.com>
2024-09-13 21:55:24 -0400

    Skin breath timer and other duration bars

--- src/TimerTracker.lua
+++ src/TimerTracker.lua
@@ -12,6 +12,18 @@ function init()
       end
     end
   end)
+
+  MirrorTimerContainer:HookScript("OnEvent", function(self, event, timerType, timeSeconds, totalTime)
+    if event ~= 'MIRROR_TIMER_START' then return end
+
+    for _, timer in pairs(self.activeTimers) do
+      timer.TextBorder:Hide()
+      timer.Text:ClearAllPoints()
+      timer.Text:SetPoint("CENTER", timer.StatusBar, "CENTER")
+      timer.Text:SetFont(STANDARD_TEXT_FONT, 11, "OUTLINE")
+      timer.Border:SetVertexColor(0, 0, 0)
+    end
+  end)
 end
 
 local frame = CreateFrame("Frame")
