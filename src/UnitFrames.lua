03a46fcf9f634871c07b1c18de310f6eb080a844
Emma Chatley <emlywind@gmail.com>
2024-09-21 22:31:32 -0400

    Remove semicolons

--- src/UnitFrames.lua
+++ src/UnitFrames.lua
@@ -36,15 +36,15 @@ OnPlayerLogin(function()
       local color = RAID_CLASS_COLORS[class]
       healthbar:SetStatusBarColor(color.r, color.g, color.b)
     elseif UnitIsPlayer(unit) and (not UnitIsConnected(unit)) then
-      healthbar:SetStatusBarColor(0.5, 0.5, 0.5);
+      healthbar:SetStatusBarColor(0.5, 0.5, 0.5)
     else
       if UnitExists(unit) then
         if (UnitIsTapDenied(unit)) and not UnitPlayerControlled(unit) then
           healthbar:SetStatusBarColor(0.5, 0.5, 0.5)
         elseif (not UnitIsTapDenied(unit)) then
-          local reaction = FACTION_BAR_COLORS[UnitReaction(unit, "player")];
+          local reaction = FACTION_BAR_COLORS[UnitReaction(unit, "player")]
           if reaction then
-            healthbar:SetStatusBarColor(reaction.r, reaction.g, reaction.b);
+            healthbar:SetStatusBarColor(reaction.r, reaction.g, reaction.b)
           end
         end
       end
