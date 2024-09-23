cc48c264c117d7b54b7c59a5334a3b5e3bb9795a
Emma Chatley <emchatley@gmail.com>
2024-09-22 23:57:05 -0400

    Testing new raidframes skinning method

--- src/RaidFrames.lua
+++ src/RaidFrames.lua
@@ -16,14 +16,17 @@ local function updateTextures(self)
         self.myHealPrediction:SetTexture(EUIDB.statusBarTexture)
         self.otherHealPrediction:SetTexture(EUIDB.statusBarTexture)
       else
-        local healthBarTexture = getBetterHealthTexture(self)
-        self.healthBar:SetStatusBarTexture(healthBarTexture)
-        local powerBarTexture = getBetterHealthTexture(self)
-        self.powerBar:SetStatusBarTexture(powerBarTexture)
-        local healPredictTexture = getBetterHealthTexture(self)
-        self.myHealPrediction:SetTexture(healPredictTexture)
-        local otherHealPredictTexture = getBetterHealthTexture(self)
-        self.otherHealPrediction:SetTexture(otherHealPredictTexture)
+        if not self.healthBar.euiTex then
+          self.healthBar.euiTex = getBetterHealthTexture(self.healthBar)
+          self.powerBar.euiTex = getBetterHealthTexture(self.powerBar)
+          self.myHealPrediction.euiTex = getBetterHealthTexture(self)
+          self.otherHealPrediction.euiTex = getBetterHealthTexture(self)
+        end
+
+        self.healthBar:SetStatusBarTexture(self.healthBar.euiTex)
+        self.powerBar:SetStatusBarTexture(self.powerBar.euiTex)
+        self.myHealPrediction:SetTexture(self.myHealPrediction.euiTex)
+        self.otherHealPrediction:SetTexture(self.otherHealPrediction.euiTex)
       end
 
       self.vertLeftBorder:Hide()
